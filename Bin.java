3d830e5ad7888e26b8ea55a14d64e3a616fe4cd8
skylarsmoke <42879587+skylarsmoke@users.noreply.github.com>
2019-03-04 11:15:40 -0700

    Update

--- Bin.java
+++ Bin.java
@@ -2,7 +2,27 @@ package Lab2;
 
 public class Bin {
 	
-	private int capacity;
+	int capacity;
+	int openSpace;
 	
+	public Bin(int capacity) {
+		this.capacity = capacity;
+	}
 
+	public void calculateSpace(int itemSize) {
+		this.openSpace = this.capacity - itemSize;
+	}
+	
+	public boolean checkFit(int itemSize) {
+		if (itemSize <= this.openSpace) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public int getOpenSpace() {
+		return this.openSpace;
+	}
+	
 }
